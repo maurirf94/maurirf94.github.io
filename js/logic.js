d786f7b199aa91bf32aef550cc8cc651f0df0d1b
Mauricio Rodriguez <maurirf94@gmail.com>
2021-08-25 21:15:39 -0300

    Mejoras de estilo y script para rellenar form

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,4 @@
-let guest=null
-var guests = [
-  {
+const guestList = [{
     "name": "Mauricio Rodriguez",
     "id": "1",
     "onlyParty": true,
@@ -14,27 +12,70 @@ var guests = [
   }
 ];
 
+loadInitialData();
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const guestId = urlParams.get('id')
 
-for (let i = 0; i < guests.length; i++) {
-  if (guests[i].id == guestId) {
-    guest = guests[i];
-  }
-}
-if (guest==null){
-    window.location.href="errorPage.html";
+function loadInitialData() {
+  var guestId = getGuestId();
+  var guest = getGuestData(guestList, guestId);
+  loadGuestData(guest);
+
 }
 
-document.getElementById("guestName").value = guest.name;
-select = document.getElementById('guestsNumber');
-if (guest != null) {
-  for (let i = 0; i < guest.guestsNumber; i++) {
-    var opt = document.createElement('option');
-    opt.value = i+1;
-    opt.innerHTML = i+1;
-    select.appendChild(opt);
+function loadGuestData(guest) {
+  if (guest != null) {
+    loadGuestName(guest);
+    loadGuestsNumber(guest);
+  } else {
+    window.location.href = "errorPage.html";
   }
+  return;
+}
+
+
+
+function getGuestData(guestList, guestId) {
+  let guest = null
+  for (let i = 0; i < guestList.length; i++) {
+    if (guestList[i].id == guestId) {
+      guest = guestList[i];
+    }
+  }
+  return guest;
+}
+
+
+function getGuestId() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const guestId = urlParams.get('id');
+  return guestId
+}
+
+
+function loadGuestName(guest) {
+  document.getElementById("guestName").value = guest.name;
+  return
+}
+
+
+function loadGuestsNumber(guest) {
+  document.getElementById("guestsNumber").innerHTML = '';
+  var attend = document.getElementById('guestAttend').value;
+  var firstOpt = document.createElement('option');
+  firstOpt.value = '';
+  firstOpt.innerHTML = 'Invitados';
+  firstOpt.className = 'first-option';
+  firstOpt.selected = true;
+  firstOpt.disabled = true;
+  document.getElementById('guestsNumber').appendChild(firstOpt);
+  if (attend != "no") {
+    for (let i = 0; i < guest.guestsNumber; i++) {
+      var opt = document.createElement('option');
+      opt.value = i + 1;
+      opt.innerHTML = i + 1;
+      document.getElementById('guestsNumber').appendChild(opt);
+    }
+  }
+  return
 }
