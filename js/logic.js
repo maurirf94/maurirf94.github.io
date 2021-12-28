d328b83dda9581d7fdb0bb51531bf2bac77df69a
Mauricio Rodriguez <maurirf94@gmail.com>
2021-12-28 20:22:06 -0300

    Modal Covid

--- js/logic.js
+++ js/logic.js
@@ -100,6 +100,20 @@ function loadEventList(guest) {
     document.getElementById("events").innerHTML = `
       <div class="container">
         <div class="row">
+          <div id="protocolModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+              <span class="close">&times;</span>
+                <h2 class="events-title">Protocolo covid</h2>
+                <ul class="ulProtocol">
+                  <li><h1 class="events-description">Para concurrir a la fiesta es obligatorio estar vacunado contra el COVID-19</h1></li>
+                  <li><h1 class="events-description">Para que todos podamos disfrutar sin preocupaciones te pedimos que:</h1></li>
+                  <li>-Lleves tapabocas.</li>
+                  <li>-Adjuntes los certificados de vacunación de quienes concurriran al momento de confirmar asistencia o nos lo envíes por whatsapp al +59898500800. </li>
+                  <li>-Lleves tu certificado a la fiesta en formato físico o digital.</li>
+                </ul>
+            </div>
+          </div>
           <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
             <div class="events">
               <h2 class="events-title">Civil</h2>
@@ -108,7 +122,7 @@ function loadEventList(guest) {
                 <h1 class="events-description">Registro Civil - Agraciada 532</h1>
                 <h1 class="events-description">Rivera, Uruguay​ ​</h1>
                 <div class="events-button">
-                  <a href="https://goo.gl/maps/D8F11KJwszPEKGrW8" target="_blank" class="btn btn-default">Cómo llegar</a>
+                  <a href="https://goo.gl/maps/DuqaE3ndJbt33Kbz6" target="_blank" class="btn btn-default">Cómo llegar</a>
                 </div>
               </div>
               <div class="events-price">
@@ -116,7 +130,7 @@ function loadEventList(guest) {
                 <h1 class="events-description">La Perdiz - Av. Sepé 51</h1>
                 <h1 class="events-description">Rivera, Uruguay​ ​</h1>
                 <div class="events-button">
-                  <a href="https://goo.gl/maps/D8F11KJwszPEKGrW8" target="_blank" class="btn btn-default">Cómo llegar</a>
+                  <a href="https://goo.gl/maps/rJgdBDUp7yyDHHVB9" target="_blank" class="btn btn-default">Cómo llegar</a>
                 </div>
               </div>
             </div>
@@ -130,22 +144,29 @@ function loadEventList(guest) {
                 <h1 class="events-description">Solar Dom Pedro​​</h1>
                 <h1 class="events-description">R. Jose Fernandes Mendes,161 ​</h1>
                 <h1 class="events-description">Armour, Livramento, Brasil​ ​</h1>
+                <h1 class="events-description">Vestimenta formal​ ​</h1>
               </div>
               <div class="events-button">
                 <a href="#rsvp" class="smoothScroll btn btn-default">Confirmar asistencia</a><br>
-                <a href="https://goo.gl/maps/g5PuB5YskT7hHGRf9" target="_blank" class="btn btn-default">Cómo llegar</a>
-
+                <a href="https://goo.gl/maps/g5PuB5YskT7hHGRf9" target="_blank" class="btn btn-default">Cómo llegar</a><br>
+                <a id="btnProtocol" class="smoothScroll btn btn-default">Protocolo COVID</a>
               </div>
             </div>
           </div>
         </div>
       </div>
     `;
-  }
-  else{
+  } else {
     document.getElementById("events").innerHTML = `
       <div class="container">
         <div class="row">
+          <div id="protocolModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+              <span class="close">&times;</span>
+              <p>Some text in the Modal..</p>
+            </div>
+          </div>
           <div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3 wow fadeInUp" data-wow-delay="0.3s">
             <div class="events">
               <h2 class="events-title">Fiesta</h2>
@@ -164,5 +185,32 @@ function loadEventList(guest) {
         </div>
       </div>
     `;
+  }
+}
+
+//Modal Script
+// Get the modal
+var modal = document.getElementById("protocolModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnProtocol");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
