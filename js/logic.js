97a5add6508a10ea8430f265c840907d2393eb4e
Mauricio Rodriguez <maurirf94@gmail.com>
2022-02-02 10:48:49 -0300

    Lista de invitatados y texto wpp

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,992 @@
 const guestList = [{
-    "name": "Mauricio Rodriguez",
-    "id": "1",
+    "name": "María de los Ángeles y Flia.",
+    "id": "420",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Juan Pedro Silva y Graciela Mendiondo",
+    "id": "426",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Gonzalo Macri",
+    "id": "650",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Florencia Franquini",
+    "id": "758",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Romina Ferraz y Mauro Cuello",
+    "id": "1682",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Celia Tasende y Ambrosio Macri",
+    "id": "2652",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Aler Donadío y Flia.",
+    "id": "87452",
+    "onlyParty": false,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Carlos Acevedo y Flia.",
+    "id": "54530",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Jaqueline Rodriguez y Esposo",
+    "id": "4239",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Horacio Giudice y Sra. ",
+    "id": "122",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Hugo Lara y Patricia",
+    "id": "89904",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Fabricio Sosa",
+    "id": "5020",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Roberto Machin",
+    "id": "5398",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Rodrigo Gutierrez",
+    "id": "7460",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Maximiliano Pereira y Soledad Baltrons",
+    "id": "7857",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Martina Pascualvaca",
+    "id": "8160",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "José Soria y Flia.",
+    "id": "4666",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Mariela Lo Cicero y Flia.",
+    "id": "89874",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Ana Ortega y Esposo",
+    "id": "9546",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Agustin Ciaccio y Treicy González",
+    "id": "9888",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Leticia Rodriguez y Matías Álvez",
+    "id": "11388",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Marisa Fernandez y Flia.",
+    "id": "11936",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "José Rodríguez y Esther Fernandez",
+    "id": "11508",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Matilde Hope",
+    "id": "11768",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Miriam Silveira",
+    "id": "70099",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Francis Zucco y Flia.",
+    "id": "13082",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Nathalie Chuy y Gabriel Lemos",
+    "id": "45566",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Francisco y Flia.",
+    "id": "14645",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Matias Fagundez",
+    "id": "14924",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Tatiana Pajoluck",
+    "id": "15297",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "César Cisterna",
+    "id": "17461",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Silvina Hernandez",
+    "id": "35523",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Fabiane Acevedo",
+    "id": "19299",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Vanesa Viera y Carlos Montiel",
+    "id": "54804",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Zully Melo y Servando Fros",
+    "id": "19425",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Claudia Duarte",
+    "id": "20750",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Gastón Pouquette",
+    "id": "20844",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Florencia Bouza y Federico Introini",
+    "id": "21239",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Rodrigo Cortés y Flia.",
+    "id": "21434",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Cecilia Di Cono",
+    "id": "22433",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Zulma Ferreiro y René de la Rosa",
+    "id": "65039",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Patricia Gomez y Gabriel Codevila",
+    "id": "23160",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Ignacio Varoli",
+    "id": "23229",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Juan Esperón y Flia.",
+    "id": "23243",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "José Ferreyra y pareja",
+    "id": "23277",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Karina Lara, Alice y Matías",
+    "id": "23636",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Ariel Gonzalez y Gilda Cecchetti",
+    "id": "23756",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Adriana Ferraz y Flia.",
+    "id": "75288",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Alejandro Bertón y Martha Huerta",
+    "id": "2984",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Santiago Barreira",
+    "id": "27706",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Pablo Fernandez y flia",
+    "id": "28879",
+    "onlyParty": false,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Luis Muslera",
+    "id": "30037",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Javier Borca",
+    "id": "30309",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Alicia Solé y Esposo",
+    "id": "63800",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Florencia Lima y Eduardo Lasso",
+    "id": "30830",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Daniel Silva y Flia.",
+    "id": "31475",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Ana María Maestro",
+    "id": "92794",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Elisa Gomez y Esposo",
+    "id": "36891",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Alina Szyman y Norberto Cánepa",
+    "id": "37132",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Antonio Bertón y Sra.",
+    "id": "47186",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Florencia Ferraro y Federico Romano",
+    "id": "37824",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Lucía Altamirano",
+    "id": "38118",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Bernardo Xavier y María Eugenia Perurena",
+    "id": "39011",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Elisa Fagundez y Gonzalo Moreira",
+    "id": "39285",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Rosina Fitipaldo y Nicolás Falqués",
+    "id": "41244",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Betty Ferraz, Silvia Cúparo y Leticia Lotito ",
+    "id": "47064",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Carlos Machado y Sra.",
+    "id": "19800",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Claudia Bertón y Josías Mora",
+    "id": "61227",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Cristina Perroni y Flia.",
+    "id": "37769",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Eduardo Bertón y Majella Lara",
+    "id": "13637",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Mary Cristina Rodriguez",
+    "id": "44893",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Eliana Cardozo y Flia.",
+    "id": "55767",
     "onlyParty": false,
     "guestsNumber": 4
   },
   {
-    "name": "Gimena Berton",
-    "id": "2",
+    "name": "José Aldabalde",
+    "id": "46361",
     "onlyParty": true,
     "guestsNumber": 2
+  },
+  {
+    "name": "Sofia Perez y Santiago Lerena",
+    "id": "46534",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Mary Nuñez",
+    "id": "46775",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Rodrigo Miranda",
+    "id": "46966",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Facundo Bertón",
+    "id": "68571",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Francisco Bertón y Flia.",
+    "id": "60332",
+    "onlyParty": false,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Pablo Mattano y Flia.",
+    "id": "47280",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Ignacio Fernández y Mabel Silvera",
+    "id": "48875",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Florencia Zunino  y Gastón Diaz",
+    "id": "48921",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Magdalena Radi y Valentín Vazquez",
+    "id": "49738",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Julio Xavier y Sra.",
+    "id": "49888",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Florencia Berón",
+    "id": "50725",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Guillermo Sosa",
+    "id": "51283",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Matías Sabalsagaray y Sra.",
+    "id": "51497",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Esteban Machin",
+    "id": "51548",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Magdalena Gramont",
+    "id": "52085",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Mario Lotito y Sra.",
+    "id": "53417",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Luis Fros",
+    "id": "53815",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Robert Silveira y Miriam Elverdín",
+    "id": "54425",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Nicolas Rodríguez",
+    "id": "54436",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Gerardo Perroni y Fabiana Veleda",
+    "id": "98077",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Hilda Huerta",
+    "id": "11430",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Magdalena Cabrera",
+    "id": "54953",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Hugo Gonzalez y Flia.",
+    "id": "91295",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Rodrigo Prado y Sra.",
+    "id": "55907",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Álvaro Fros y Sra.",
+    "id": "58539",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Hugo Solé y Sra.",
+    "id": "94492",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Juan José Barrios",
+    "id": "60971",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Martha Rodriguez",
+    "id": "61076",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "José Bertón y Serrana Fernandez Goñi",
+    "id": "73394",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Felipe Longa y Sra.",
+    "id": "62466",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Bernardo Cabrera",
+    "id": "63672",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Lilian Muslera y Nubia Muslera",
+    "id": "88874",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Diego Chagas",
+    "id": "64569",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Lucas Machado",
+    "id": "9109",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Joaquín Giaccio y Sra.",
+    "id": "65823",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Santiago Arevalo y Flia.",
+    "id": "66610",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Leonardo Madera y Flia.",
+    "id": "66894",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Sebastian Machín y Flia.",
+    "id": "67130",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Andrés Clavier y Alexandra Muníz",
+    "id": "67512",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Luis Alberto Huerta y Sra.",
+    "id": "8252",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Matias Mendez",
+    "id": "68704",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Luciana Prestes y Romain Ruel",
+    "id": "68929",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Enrique Arévalo y Flia.",
+    "id": "68962",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Cristina Normey",
+    "id": "69519",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Marcelo Artigas",
+    "id": "42448",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "María del Rosario Risso y Esposo",
+    "id": "44076",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Gustavo Muslera y Flia.",
+    "id": "72608",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Renato Perurena y Flia.",
+    "id": "72649",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Nicolas Gonzalez",
+    "id": "72841",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Romel Barris y Judith Scarpelli",
+    "id": "73093",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Mariela Muslera y Flia.",
+    "id": "23878",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Marilú y Carolina",
+    "id": "4538",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Matilde Costa y Flia.",
+    "id": "98430",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Mariela Rodriguez",
+    "id": "76658",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Carlos Martinez y Sra.",
+    "id": "80885",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Fernando Barrios y Sra.",
+    "id": "81250",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Martín Tasende y Flia.",
+    "id": "81831",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Santiago Dorregui y Sra.",
+    "id": "82878",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Gabriel Nuñez",
+    "id": "83427",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Ignacio Norry y Flia.",
+    "id": "85015",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Lucía Teixeira y Gastón Utrico",
+    "id": "85053",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Juan Pablo Valentín",
+    "id": "85116",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Mauricio Rodriguez y Gimena Bertón",
+    "id": "25496",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Rita Carneiro y Flia.",
+    "id": "88426",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Natalia Lotito y Flia.",
+    "id": "74799",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Martha, Mirna, y Luci",
+    "id": "88924",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Nicolas Perroni",
+    "id": "321",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Noelia Modernel y Flia.",
+    "id": "71956",
+    "onlyParty": true,
+    "guestsNumber": 4
+  },
+  {
+    "name": "Roberto Piriz y Sra.",
+    "id": "91011",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Olga Bonilla",
+    "id": "42061",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Jimena Olivera y Flia.",
+    "id": "91740",
+    "onlyParty": true,
+    "guestsNumber": 3
+  },
+  {
+    "name": "Roberto Machado y Mariela Louzan",
+    "id": "30813",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Roberto Perroni y Guadalupe Maciel",
+    "id": "92769",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Germán Rodriguez y Romina Kalichman",
+    "id": "94314",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Ruben Ferraz y Flia.",
+    "id": "42290",
+    "onlyParty": true,
+    "guestsNumber": 5
+  },
+  {
+    "name": "Pablo Longa y Sra.",
+    "id": "94574",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Álvaro Vaamonde",
+    "id": "94589",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Débora López",
+    "id": "95143",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Agustina Risso y Esposo",
+    "id": "95543",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Giorgina Scarpa y Gerardo Ribeiro",
+    "id": "95707",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Gianela Costa y Rodrigo Gaye",
+    "id": "97739",
+    "onlyParty": false,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Ruben Piñeyro y Flia.",
+    "id": "98590",
+    "onlyParty": true,
+    "guestsNumber": 6
+  },
+  {
+    "name": "Silvana Muslera",
+    "id": "22753",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Sirley Berton y Flia.",
+    "id": "99827",
+    "onlyParty": true,
+    "guestsNumber": 6
+  },
+  {
+    "name": "Francisco Calvo y Sra.",
+    "id": "98730",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Nicolás Coniglio",
+    "id": "98822",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Susana Freire",
+    "id": "18336",
+    "onlyParty": true,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Tania Huerta",
+    "id": "3001",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Maira González y Maximiliano Zorozabal",
+    "id": "418",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Walter Huerta",
+    "id": "42592",
+    "onlyParty": false,
+    "guestsNumber": 1
+  },
+  {
+    "name": "Hellen Guidobono",
+    "id": "305",
+    "onlyParty": true,
+    "guestsNumber": 2
+  },
+  {
+    "name": "Marilyn Rodriguez",
+    "id": "303",
+    "onlyParty": true,
+    "guestsNumber": 1
   }
 ];
 
