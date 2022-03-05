0f05c1d7340cc3687535543ef5fa734510f8219c
Mauricio Rodriguez <maurirf94@gmail.com>
2022-03-05 10:54:59 -0300

    Corrección invitados

--- js/logic.js
+++ js/logic.js
@@ -1005,23 +1005,6 @@ const guestList = [{
     "id": "307",
     "onlyParty": true,
     "guestsNumber": 1
-  },
-  {
-    "name": "Alejandro Gómez y Sra.",
-    "id": "308",
-    "onlyParty": true,
-    "guestsNumber": 2
-  },
-  {
-    "name": "Vicente Correa",
-    "id": "54805",
-    "onlyParty": true,
-    "guestsNumber": 1
-  },
-    "name": "Joaquin Dellepiane",
-    "id": "54809",
-    "onlyParty": true,
-    "guestsNumber": 1
   }
 ];
 
